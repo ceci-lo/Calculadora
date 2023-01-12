717a84bd4745edf852ce40dff6dc40a904825703
cecilia <lcecilia8147@gmail.com>
2023-01-12 16:59:24 -0300

    se crea funcion resta

--- script.js
+++ script.js
@@ -1,18 +1,53 @@
-let cifra = "";
-let acc = 0;
+let cifra = ""; // valor actual
+let acc = 0; // se acumulan los valores que se teclean
+let sumar = false;
+let restar = false;
+let pOperacion = true;
+
 function displayNumeros(numeros) {
   document.getElementById("display").value = cifra + numeros;
   cifra = document.getElementById("display").value;
 }
 
 function suma() {
-  acc = acc + parseInt(cifra);
-  document.getElementById("display").value = acc;
+  if (restar) {
+    acc = acc - parseInt(cifra);
+    document.getElementById("display").value = acc;
+  } else {
+    acc = acc + parseInt(cifra);
+    document.getElementById("display").value = acc;
+  }
   cifra = "";
+  sumar = true;
+  restar = false;
+  pOperacion = false;
 }
+
 function resta() {
-  let max = Math.max(acc, parseInt(cifra));
-  acc = max - acc;
-  document.getElementById("display").value = acc;
+  if (pOperacion == false) {
+    if (sumar) {
+      acc = acc + parseInt(cifra);
+      document.getElementById("display").value = acc;
+    } else {
+      acc = acc - parseInt(cifra);
+      document.getElementById("display").value = acc;
+    }
+  } else {
+    acc = parseInt(cifra);
+    pOperacion = false;
+  }
+
   cifra = "";
+  restar = true;
+  sumar = false;
+}
+
+function resultado() {
+  if (sumar) {
+    document.getElementById("display").value = acc + parseInt(cifra);
+  } else if (restar) {
+    document.getElementById("display").value = acc - parseInt(cifra);
+  }
+  acc = parseInt(document.getElementById("display").value);
+  cifra = 0;
 }
